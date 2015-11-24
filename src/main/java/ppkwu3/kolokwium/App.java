5df8f61f71dbd9a2e602a446eb23fd744ed74887
GorskiJakub <jakubg93@o2.pl>
2015-11-24 14:28:23 +0100

    first commit

---
+++ src/main/java/ppkwu3/kolokwium/App.java
@@ -0,0 +1,13 @@
+package ppkwu3.kolokwium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
