6f66377477c3f3aceab58eb06a43b2666b26d6a6
GorskiJakub <jakubg93@o2.pl>
2015-11-24 15:31:58 +0100

    dzialajace rozpoznawanie plikow i wyswietlanie ich, problemy przy deszyfrowaniu

--- src/main/java/ppkwu3/kolokwium/App.java
+++ src/main/java/ppkwu3/kolokwium/App.java
@@ -1,13 +1,41 @@
 package ppkwu3.kolokwium;
 
-/**
- * Hello world!
- *
- */
+
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+import bibliotekaSzyfr.Szyfr;
+import biblioteki.FileTypeDetermine;
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException, IOException
     {
-        System.out.println( "Hello World!" );
+    	System.out.println("Podaj sciezke do pliku");
+    	Scanner s = new Scanner(System.in);
+    	String pathToCheckType = s.next();
+    	
+    	FileTypeDetermine.getFileTypeFromPath(pathToCheckType);
+    	try {
+			FileTypeDetermine.previewFile(pathToCheckType);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	String FileName = "encryptedtext.txt";
+        String FileName2 = "decryptedtext.txt";
+        String pass = "test1";
+        String secretID="sekret";
+    	
+    	//Szyfr.szyfrowanie(FileName, pass, secretID );
+	    //Szyfr.deszyfrowanie(FileName2, pass, secretID);
+    	
     }
 }
